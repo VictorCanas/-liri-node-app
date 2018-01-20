f63bb5b183f06fd16f692ad39ffb58c80b8f47d1
Victor <victorcanasint@gmail.com>
2018-01-20 16:30:27 -0500

    twitter function

--- liri.js
+++ liri.js
@@ -13,16 +13,12 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-//Make it so liri.js can take in one of the following commands
-//my-tweets
-//spotify-this-song
-//movie-this
-//do-what-it-says
-
 //Workflow process
 //1. let's try to get input from a movie first and put that inside a function- DONE
 //2. let's do else if statemets to run different functions - DONE
 //3. Test each else if statement by doing console.log on each individually based on an command argument
+//4. Create my-tweets function - DONE
+//5. Crate mySpotify function 
 
 //test to run when process argv is movie-this
 var command = process.argv[2];
@@ -31,49 +27,64 @@ var nodeArgs = process.argv.slice(3).join(" ");
 //console.log(nodeArgs);
 
 if (command === "my-tweets") {
-	//myTweets();
+	myTweets();
 	//testing
-	console.log("twitter");
+	//console.log("twitter");
 } else if (command === "spotify-this-song") { 
-	//mySpotify();
+	mySpotify();
 	//testing
-	console.log("spotify");
+	//console.log("spotify");
 } else if (command === "movie-this") {
 	movieThis();
 	//testing
-	console.log("movies");
+	//console.log("movies");
 } else if (command === "do-what-it-says") {
 	//doThis();
 	//testing
-	console.log("do");
+	//console.log("do");
 }
 
+
+function myTweets() {
+
+	//only gets the last 20 tweets
+	var params = {screen_name: 'V70051141', count: 20};
+
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+			if (!error) {
+				for (var i = 0; i < tweets.length; i++) {
+					console.log("\n" + tweets[i].text);
+				}
+			} else {
+			console.log(error);
+		}
+	});
+};
 
 
 //movie funciton 
 function movieThis() {
 
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + nodeArgs + "&y=&plot=short&apikey=trilogy";
+	// Then run a request to the OMDB API with the movie specified
+	var queryUrl = "http://www.omdbapi.com/?t=" + nodeArgs + "&y=&plot=short&apikey=trilogy";
 
-request(queryUrl, function(error, response, body) {
+	request(queryUrl, function(error, response, body) {
 
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
+	  // If the request is successful
+	  if (!error && response.statusCode === 200) {
 
-    // Parse the body of the site and export information 
-    console.log("Title of the movie: " + JSON.parse(body).Title);
-    console.log("Released year: " + JSON.parse(body).Year);
-    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-    //fix exporting rotten tomatoes 
-    console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
-    console.log("Country the movie was released: " + JSON.parse(body).Country);
-    console.log("Language of the movie: " + JSON.parse(body).Language);
-    console.log("Plot of the movie: " + JSON.parse(body).Plot);
-    console.log("Actors: " + JSON.parse(body).Actors);
-  }
+	    // Parse the body of the site and export information 
+	    console.log("Title of the movie: " + JSON.parse(body).Title);
+	    console.log("Released year: " + JSON.parse(body).Year);
+	    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+	    console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+	    console.log("Country the movie was released: " + JSON.parse(body).Country);
+	    console.log("Language of the movie: " + JSON.parse(body).Language);
+	    console.log("Plot of the movie: " + JSON.parse(body).Plot);
+	    console.log("Actors: " + JSON.parse(body).Actors);
+	  }
 
-});
+	});
 };
 
 
