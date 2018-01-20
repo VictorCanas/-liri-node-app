12cb47f7de3b4ee2f153d671234650ad86bcaeaa
Victor <victorcanasint@gmail.com>
2018-01-20 16:09:22 -0500

    liri

--- liri.js
+++ liri.js
@@ -1,9 +1,17 @@
 //reads and set any environment variables with the dotenv package
 require("dotenv").config();
 
+// Include the npm packages (Don't forget to run "npm install" in this folder first!)
+// done importing
+var fs = require("fs");
+var request = require("request");
+var Spotify = require("node-spotify-api");
+var Twitter = require("twitter");
+var keys = require("./keys.js");
+
 //code required to import the keys.js file and store it in a variable
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
 //Make it so liri.js can take in one of the following commands
 //my-tweets
@@ -11,43 +19,42 @@ require("dotenv").config();
 //movie-this
 //do-what-it-says
 
-//let's try to get input from a movie first 
+//Workflow process
+//1. let's try to get input from a movie first and put that inside a function- DONE
+//2. let's do else if statemets to run different functions - DONE
+//3. Test each else if statement by doing console.log on each individually based on an command argument
 
-// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
-var request = require("request");
-
-// Store all of the arguments in an array
+//test to run when process argv is movie-this
 var command = process.argv[2];
-
-//testing the output of command to see if it works with a -
 //console.log(command);
+var nodeArgs = process.argv.slice(3).join(" ");
+//console.log(nodeArgs);
 
-var nodeArgs = process.argv;
-
-// Create an empty variable for holding the movie name
-var movieName = "";
-
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 2; i < nodeArgs.length; i++) {
-
-  if (i > 2 && i < nodeArgs.length) {
-
-    movieName = movieName + "+" + nodeArgs[i];
-
-  }
-
-  else {
-
-    movieName += nodeArgs[i];
-
-  }
+if (command === "my-tweets") {
+	//myTweets();
+	//testing
+	console.log("twitter");
+} else if (command === "spotify-this-song") { 
+	//mySpotify();
+	//testing
+	console.log("spotify");
+} else if (command === "movie-this") {
+	movieThis();
+	//testing
+	console.log("movies");
+} else if (command === "do-what-it-says") {
+	//doThis();
+	//testing
+	console.log("do");
 }
 
 
 
+//movie funciton 
+function movieThis() {
+
 // Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+var queryUrl = "http://www.omdbapi.com/?t=" + nodeArgs + "&y=&plot=short&apikey=trilogy";
 
 request(queryUrl, function(error, response, body) {
 
@@ -63,10 +70,14 @@ request(queryUrl, function(error, response, body) {
     console.log("Country the movie was released: " + JSON.parse(body).Country);
     console.log("Language of the movie: " + JSON.parse(body).Language);
     console.log("Plot of the movie: " + JSON.parse(body).Plot);
-    console.log("Plot of the movie: " + JSON.parse(body).Actors);
+    console.log("Actors: " + JSON.parse(body).Actors);
   }
-  
+
 });
+};
+
+
+
 
 
 
